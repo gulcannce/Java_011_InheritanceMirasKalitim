01ed61d4edcdf332bf38ed2673a04eb4b1c7a041
gulcannce <gulcanncee@gmail.com>
2024-11-08 19:29:14 +0300

    Initial commit

---
+++ src/main/java/com/gulcancelik/departman/Muhasebeci.java
@@ -0,0 +1,23 @@
+package com.gulcancelik.departman;
+
+import com.mimaraslan.base.Personel;
+import lombok.*;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Muhasebeci extends Personel {
+    /*
+    private String adi;
+    private String soyadi;
+    private String telefonNo;
+    private String kimlikNo;
+    private String hesapNo;
+    private int personelNo;
+    private String departmani;
+    */
+    private String unvani;
+    private long sertifikaNo;
+}
